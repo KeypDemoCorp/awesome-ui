d2fa7e4bd01765ad1008bd1d75d6aa160cd6a530
Arnaud <arnaud.lachaume@keypup.io>
2020-02-20 11:01:53 +0100

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Fake build script
+
+# Simulate build time
+echo "Running tests..."
+sleep 10
+
+if [[ "$(cat BUILD_RESULT.txt)" == "success" ]]; then
+  echo "Tests are successful"
+  exit 0
+else
+  echo "Tests failed (content of BUILD_RESULT.txt is different from 'success')"
+  exit 1
+fi
