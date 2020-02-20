7ae537b852f3bd9a41d33e407e8ec73293108199
Arnaud <arnaud.lachaume@keypup.io>
2020-02-20 11:07:33 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 # Fake build script
 
 # Simulate build time
 echo "Running tests..."
 sleep 10
 
-if [[ "$(cat BUILD_RESULT.txt)" == "success" ]]; then
+if [ "$(cat BUILD_RESULT.txt)" == "success" ]; then
   echo "Tests are successful"
   exit 0
 else
